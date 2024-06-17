0591197cb9cd57aeee53c81b41dd636017cea263
Pierre Carrier <pc@rrier.fr>
2024-06-17 08:44:47 +0200

    luaaa

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,85 @@
+use std::collections::HashMap;
+use std::env;
+use std::env::args;
+use std::error::Error;
+use std::path::PathBuf;
 use std::time::Instant;
 
-use mimalloc::MiMalloc;
-use reqwest::Client;
-use rquickjs::{AsyncContext, AsyncRuntime, Module};
-use sqlite::Value;
+use http_cache_reqwest::{CACacheManager, Cache, CacheMode, HttpCache, HttpCacheOptions};
+use mlua::{Lua, LuaOptions, MultiValue, StdLib};
+use reqwest::{Client, Url};
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
 
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
+struct Meta {
+    home: PathBuf,
+    base: Url,
+    tool: Url,
+    fragment: String,
+    args: Vec<String>,
+    env: HashMap<String, String>,
+}
 
 struct YasRT {
-    pub ctx: AsyncContext,
-    pub conn: sqlite::Connection,
-    pub http_client: Client,
+    pub meta: Meta,
+    pub lua: Lua,
+    pub http_client: ClientWithMiddleware,
 }
 
 impl YasRT {
-    async fn new() -> Result<Self, Box<dyn std::error::Error>> {
-        let rt = AsyncRuntime::new()?;
-        let ctx = AsyncContext::full(&rt).await?;
-        let conn = sqlite::Connection::open(":memory:")?;
-        let client = Client::new();
-        Ok(Self { ctx, conn, http_client: client })
+    async fn new(args: Vec<String>, env: HashMap<String, String>) -> Result<Self, Box<dyn Error>> {
+        let base = Url::parse(env.get("YAS_BASE").unwrap_or(&"https://oh.yas.tools".to_string()))?;
+        let tool_ref = if args.len() > 0 { args[0].as_str() } else { "repl" };
+        let args = args[1..].to_vec();
+        let (tool, fragment) = urlize(base.clone(), tool_ref)?;
+        let local = dirs::data_local_dir().ok_or("no local data dir")?;
+        let home = local.join("yas.tools");
+        let manager = CACacheManager { path: home.join("http-cache") };
+        let cache = HttpCache {
+            mode: CacheMode::Default,
+            manager,
+            options: HttpCacheOptions::default(),
+        };
+        let meta = Meta {
+            base,
+            home,
+            tool,
+            fragment,
+            args,
+            env,
+        };
+        Ok(Self {
+            meta,
+            lua: Lua::new_with(StdLib::ALL_SAFE, LuaOptions::default())?,
+            http_client: ClientBuilder::new(Client::new()).with(Cache(cache)).build(),
+        })
     }
 }
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let init = Instant::now();
-    let rt = YasRT::new().await?;
+    let rt = YasRT::new(args().skip(1).collect(), env::vars().collect()).await?;
     let setup = Instant::now();
-    let body = rt.http_client.get("https://oh.yas.tools/3")
-        .send().await?.text().await?;
+    print!("fetching {}\n", rt.meta.tool);
+    let body = rt.http_client
+        .get(rt.meta.tool.as_str())
+        .send().await?
+        .text().await?;
     let fetched = Instant::now();
-    let mut bound = rt.conn.prepare("SELECT ?")?
-        .into_iter()
-        .bind((1, body.as_str()))?;
-    bound.next();
-    let queried: Instant;
-    let evaled: Instant;
-    match bound.read(0).unwrap() {
-        Value::String(body) => {
-            queried = Instant::now();
-            let v = rt.ctx.with(|ctx| {
-                Module::evaluate(ctx, "test", body.as_bytes())?.finish::<()>()
-            }).await?;
-            evaled = Instant::now();
-            println!("{:?}", v)
-        }
-        v => panic!("unexpected value {v:?}"),
-    }
-    println!("setup: {:?}, fetch: {:?}, query: {:?}, eval: {:?}",
-             setup - init, fetched - setup, queried - fetched, evaled - queried);
+    let v = rt.lua
+        .load(body.as_str())
+        .eval::<MultiValue>()?;
+    let evaled = Instant::now();
+    println!(
+        "setup: {:?}, fetch: {:?}, eval: {:?}",
+        setup - init,
+        fetched - setup,
+        evaled - fetched
+    );
+    println!("{:?}", v);
     Ok(())
+}
+
+fn urlize(root: Url, input: &str) -> Result<(Url, String), Box<dyn Error>> {
+    root.join(input).map(|it| (it, "".to_string())).map_err(Into::into)
 }
